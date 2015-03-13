f4113c6df240a9c7f9f79ec1b2796e15c5bd0683
Damien Lebrun <dinoboff@hotmail.com>
2015-03-13 15:23:10 +0000

    Remove individual icons.

--- src/shared/components/icons/icons.js
+++ src/shared/components/icons/icons.js
@@ -10,7 +10,7 @@
    * 0. go to https://icomoon.io/app.
    * 1. Select icons from the "Material Design Icons" collection.
    * 2. download and extract the set.
-   * 3. copy svgdefs.svg and SVG into this component folder
+   * 3. copy svgdefs.svg into this component folder
    *    (replace the existing ones).
    * 4. open svgdefs.svg and replace all the `symbols` tags for `g` tags.
    * 5. open svgdefs.svg and rename all icon ids from "icon-something" to just
