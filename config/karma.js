61f38e48d1eba56f2a6f981145764da7e4bc3e3c
Damien Lebrun <dinoboff@hotmail.com>
2015-04-06 00:57:33 +0100

    Add coverage

--- config/karma.js
+++ config/karma.js
@@ -56,12 +56,20 @@ module.exports = function(config) {
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {},
+    preprocessors: {
+      'src/!(vendor)/**/*.js': 'coverage'
+    },
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress', 'coverage', 'coveralls'],
+
+    coverageReporter: {
+      type: 'lcov',
+      dir: 'coverage',
+      file: 'lcov.info'
+    },
 
     // web server port
     port: 9876,
