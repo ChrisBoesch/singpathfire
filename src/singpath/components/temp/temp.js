dfe5ff45ece978bb7c3c8911afb02cc2b464b8ff
Chris Boesch <cboesch@smu.edu.sg>
2015-04-11 13:13:52 +0800

    Adding controller methods for Jasmine testing.

--- src/singpath/components/temp/temp.js
+++ src/singpath/components/temp/temp.js
@@ -25,12 +25,20 @@
   ]).
 
   /**
-   * TempCtrl
+   * TempCtrl for Jasmine controller test experiements.
    *
    */
   controller('TempCtrl', [
     function TempCtrl() {
       this.name = 'Chris';
+      
+      this.add = function(a,b){
+        return a + b;
+      };
+
+      this.subtract = function(a,b){
+        return a - b;
+      };
     }
   ]);
 
