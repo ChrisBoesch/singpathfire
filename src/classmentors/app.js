61795ea2a26514a45f04863073cfc7af67d1caa3
Damien Lebrun <dinoboff@hotmail.com>
2015-06-02 01:51:57 +0100

    Show submitted links
    fixes #30

--- src/classmentors/app.js
+++ src/classmentors/app.js
@@ -1194,7 +1194,7 @@
         scope: false,
         require: 'ngModel',
         link: function cmContainsPostLink(scope, e, attr, model) {
-          var pattern;
+          var pattern = scope.$eval(attr.cmContains);
 
           scope.$watch(attr.cmContains, function(value) {
             pattern = value;
